c5423400883f728ae1c8afd79e5fb379a41b45b1
AlexMykulych <alexander.mykulych@gmail.com>
2021-08-26 16:53:33 +0300

    Add vue-component project

---
+++ libraries/vue-component/src/main.ts
@@ -0,0 +1,19 @@
+// register vue composition api globally
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from 'virtual:generated-pages'
+import App from './App.vue'
+
+// windicss layers
+import 'virtual:windi-base.css'
+import 'virtual:windi-components.css'
+import './styles/main.css'
+import 'virtual:windi-utilities.css'
+
+const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+app.use(router)
+app.mount('#app')
