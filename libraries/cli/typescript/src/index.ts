26482411a82dfc82449066ac790a9c81af3e4696
AlexanderMykulych <alexander.mykulych@gmail.com>
2021-12-22 22:00:47 +0200

    init coya-ts

--- libraries/cli/typescript/src/index.ts
+++ libraries/cli/typescript/src/index.ts
@@ -1,6 +1,5 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import figlet from "figlet";
+import picocolor from "picocolors";
+
+
+console.log(picocolor.green(figlet.textSync("Coya TS")));
