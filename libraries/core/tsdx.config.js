7ba16587976f13c790958312a1fd0b6d4b2bb5b5
AlexanderMykulych <alexander.mykulych@gmail.com>
2021-12-22 21:35:03 +0200

    Add typescript cli proj

--- libraries/core/tsdx.config.js
+++ libraries/core/tsdx.config.js
@@ -1,7 +1,17 @@
+const replace = require('@rollup/plugin-replace');
+
 // tsdx.config.js
 module.exports = {
     rollup(config, options) {
         config.external = (id) => id === "vue";
+        config.plugins = config.plugins.map(p =>
+            p.name === 'replace'
+              ? replace({
+                  'process.env.NODE_ENV': JSON.stringify(opts.env),
+                  preventAssignment: true,
+                })
+              : p
+          );
         return config;
     },
 };
