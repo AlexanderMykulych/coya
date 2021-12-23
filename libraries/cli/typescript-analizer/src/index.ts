cd9ea481187f16871909a6d2218331a96c50c3f4
AlexanderMykulych <alexander.mykulych@gmail.com>
2021-12-23 11:01:28 +0200

    update

--- libraries/cli/typescript-analizer/src/index.ts
+++ libraries/cli/typescript-analizer/src/index.ts
@@ -1,6 +1,5 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+export function analize(path: string) {
+    return {
+
+    };
+}
