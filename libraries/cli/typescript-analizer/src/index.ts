7b45b370e39d5c4bcdf6c84e54e99ca19633de5e
AlexanderMykulych <alexander.mykulych@gmail.com>
2021-12-23 11:10:58 +0200

    prev

--- libraries/cli/typescript-analizer/src/index.ts
+++ libraries/cli/typescript-analizer/src/index.ts
@@ -1,5 +1,5 @@
 export function analize(path: string) {
     return {
-
+        blocks: []
     };
 }
--- libraries/cli/typescript-analizer/test/blah.test.ts
+++ libraries/cli/typescript-analizer/test/blah.test.ts
@@ -1,7 +1,8 @@
-import { sum } from '../src';
+import { expect, test } from "vitest";
+import { analize } from '../src';
 
-describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
+test('analize core', () => {
+    const result = analize("./../../../core");
+    console.log(result);
+    expect(result).not.null;
 });
