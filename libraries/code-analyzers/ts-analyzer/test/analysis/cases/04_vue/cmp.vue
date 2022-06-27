b606247d91c7a666638169e4a2d756ad6bebd856
Alexander Mykulych <alexander.mykulych@gmail.com>
2022-06-27 19:54:07 +0300

    chore: add vue example

---
+++ libraries/code-analyzers/ts-analyzer/test/analysis/cases/04_vue/cmp.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
---
+++ libraries/code-analyzers/ts-analyzer/test/analysis/cases/04_vue/main.ts
@@ -0,0 +1,3 @@
+import cmp from 'cmp.vue'
+
+export const cmpFunc = () => cmp
