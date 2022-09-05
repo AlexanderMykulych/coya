6d2d7b5e00da4d6125597cb84fdb126adfb772a1
Alexander Mykulych <alexander.mykulych@gmail.com>
2022-09-05 19:13:26 +0300

    chore

--- libraries/code-analyzers/mental-project/src/components/QueryExecutor.vue
+++ libraries/code-analyzers/mental-project/src/components/QueryExecutor.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { useNeo4j, QueryResult, generateCoyaFromGraphResult } from 'coya-ts-analyzer/browser'
+import { useNeo4j, generateCoyaFromGraphResult } from 'coya-ts-analyzer/browser'
 import Coya from 'coya-vue-component'
 import 'coya-vue-component/dist/style.css'
 
@@ -10,26 +10,30 @@ const query = computed({
   set: (val) => emits('update:query', val),
 })
 
-const queryResult = ref<QueryResult | null>(null)
-
 const db = useNeo4j()
 
-const executeQuery = async () => {
-  queryResult.value = null
-  queryResult.value = await db.read(query.value)
-}
+const queryResult = useAsyncState(() => db.read(query.value), null, {
+  immediate: false,
+  resetOnExecute: true,
+})
 
-const coya = computedAsync(() => queryResult.value ? generateCoyaFromGraphResult(queryResult.value) : null)
+
+const coyaProcessing = ref(false)
+const coya = computedAsync(
+  () => queryResult.isReady && queryResult.state?.value ? generateCoyaFromGraphResult(queryResult.state.value) : null,
+  null,
+  coyaProcessing,
+)
 </script>
 
 <template>
   <div class="h-full flex" pb-5>
-    <div class="w-full h-full flex-col">
+    <div class="w-40% h-full flex-col">
       <div class="flex justify-start mb-2 pb-2" b="b-0.5 coolGray">
         <button
           class="btn"
           :disabled="!query"
-          @click="executeQuery"
+          @click="queryResult.execute()"
         >Execute</button>
       </div>
       <div class="h-95%">
@@ -38,9 +42,15 @@ const coya = computedAsync(() => queryResult.value ? generateCoyaFromGraphResult
         />
       </div>
     </div>
-    <div class="w-full">
+    <div class="w-full flex justify-center align-center">
+      <template v-if="queryResult.isLoading.value || coyaProcessing">
+        <div self-center flex="~ col">
+          <span class="loader ml-4 mb-3"></span>
+          ...{{ coyaProcessing ? 'Processing result' : 'Loading'}}
+        </div>
+      </template>
       <Coya
-        v-if="coya"
+        v-else-if="coya"
         class="w-full h-90"
         id="diagram"
         :config="coya"
@@ -49,3 +59,25 @@ const coya = computedAsync(() => queryResult.value ? generateCoyaFromGraphResult
     </div>
   </div>
 </template>
+
+<style scoped>
+.loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  display: inline-block;
+  border-top: 3px solid black;
+  border-right: 3px solid transparent;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+} 
+</style>
