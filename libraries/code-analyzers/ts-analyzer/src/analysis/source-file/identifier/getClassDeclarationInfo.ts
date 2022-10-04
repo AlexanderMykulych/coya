f058755b09879eb6903935b869df6211d7ec8eb1
Alexander Mykulych <alexander.mykulych@gmail.com>
2022-10-04 18:24:02 +0300

    chore

--- libraries/code-analyzers/ts-analyzer/src/analysis/source-file/identifier/getClassDeclarationInfo.ts
+++ libraries/code-analyzers/ts-analyzer/src/analysis/source-file/identifier/getClassDeclarationInfo.ts
@@ -1,7 +1,7 @@
-import type { ClassDeclaration, MethodDeclaration } from "ts-morph";
-import { EntityType, FunctionEntity, CodeInfoType, BaseEntity } from "../../types";
+import type { ClassDeclaration } from "ts-morph";
+import { EntityType, CodeInfoType, BaseEntity } from "../../types";
 import { getLocation } from "./getLocation";
-import { getParentId, getParentsInfo } from "./getParentId";
+import { getParentId } from "./getParentId";
 
 export function getClassDeclarationInfo(node: ClassDeclaration): BaseEntity {
   const sourceFile = node.getSourceFile()
