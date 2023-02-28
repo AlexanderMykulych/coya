d7e38934d36e897f8ebbc0e28f0d558c318514d4
Alexander Mykulych <alexander.mykulych@gmail.com>
2023-02-28 18:56:04 +0200

    edit vite config

--- libraries/connectors/gitlab/vite.config.ts
+++ libraries/connectors/gitlab/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, './index.ts'),
-      name: 'coya-youtrack-connector',
+      name: 'coya-gitlab-connector',
       // the proper extensions will be added
       fileName: 'index',
     },
