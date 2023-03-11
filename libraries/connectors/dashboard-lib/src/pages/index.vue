a33dac89ebdb65c8a41ba3b74151e398cc701fa7
Alexander Mykulych <alexander.mykulych@gmail.com>
2023-03-11 13:27:05 +0200

    add dashboard lib

---
+++ libraries/connectors/dashboard-lib/src/pages/index.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+
+const name = $ref('')
+
+const router = useRouter()
+const go = () => {
+  if (name)
+    router.push(`/hi/${encodeURIComponent(name)}`)
+}
+</script>
+
+<template>
+  <div>
+    <div i-carbon-campsite text-4xl inline-block />
+    <p>
+      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
+        Vitesse Lite
+      </a>
+    </p>
+    <p>
+      <em text-sm op75>Opinionated Vite Starter Template</em>
+    </p>
+
+    <div py-4 />
+
+    <TheInput
+      v-model="name"
+      placeholder="What's your name?"
+      autocomplete="false"
+      @keydown.enter="go"
+    />
+
+    <div>
+      <button
+        class="m-3 text-sm btn"
+        :disabled="!name"
+        @click="go"
+      >
+        Go
+      </button>
+    </div>
+  </div>
+</template>
